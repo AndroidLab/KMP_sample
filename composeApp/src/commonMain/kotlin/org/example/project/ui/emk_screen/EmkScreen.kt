d442f10c8c5dea27936fd2ad7d9c479e24c23032
ALab <a.lab.develop@mail.ru>
2024-05-27 17:52:13 +0300

    Добавил нижнюю панель

--- composeApp/src/commonMain/kotlin/org/example/project/ui/emk_screen/EmkScreen.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/ui/emk_screen/EmkScreen.kt
@@ -1,6 +1,11 @@
 package org.example.project.ui.emk_screen
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberDraggableState
+import androidx.compose.foundation.gestures.rememberScrollableState
+import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,17 +16,22 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
+import kotlinx.coroutines.launch
 import org.example.project.ui.main_screen.data.models.BirdModel
 
 /**
@@ -55,12 +65,22 @@ fun EmkScreen(viewModel: EmkViewModel) {
                 }
             }
         }
+        val scrollState = rememberLazyGridState()
+        val coroutineScope = rememberCoroutineScope()
         AnimatedVisibility(uiState.selectedImages.isNotEmpty()) {
             LazyVerticalGrid(
+                state = scrollState,
+                modifier = Modifier.fillMaxSize().padding(horizontal = 5.dp).draggable(
+                    orientation = Orientation.Vertical,
+                    state = rememberDraggableState { delta ->
+                        coroutineScope.launch {
+                            scrollState.scrollBy(-delta)
+                        }
+                    },
+                ),
                 columns = GridCells.Fixed(2),
                 horizontalArrangement = Arrangement.spacedBy(5.dp),
                 verticalArrangement = Arrangement.spacedBy(5.dp),
-                modifier = Modifier.fillMaxSize().padding(horizontal = 5.dp),
                 content = {
                     items(uiState.selectedImages) {
                         BirdImageCell(it)
